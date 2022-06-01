036f325cedd2cc2d1f05f29c8eb6321461cfbcde
harsha <harshs13dec@gmail.com>
2022-06-01 05:50:24 +0000

    sample Java project

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,13 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Harsha. This is my first line of code I have written!" );
+    }
+}
