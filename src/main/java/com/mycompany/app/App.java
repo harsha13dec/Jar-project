5e1c2e3ed65c107b859ce92f3cbabe8956f2d9d0
harsha13dec <105868260+harsha13dec@users.noreply.github.com>
2022-06-01 11:50:37 +0530

    updated the code
    updated the code

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello Harsha. This is my first line of code I have written!" );
+        System.out.println( "Hello Harsha. This is my second line of code I have written!" );
     }
 }
