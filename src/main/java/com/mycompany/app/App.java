890f7076ac89e36c9324c5dd30bf825d70d7f714
harsha13dec <105868260+harsha13dec@users.noreply.github.com>
2022-07-27 11:15:45 +0530

    Added a new line of code

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello Harsha. This is my first line of code I have written!" );
+        System.out.println( "Hello Harsha. you are combating a single player game and you are doing really well!" );
     }
 }
